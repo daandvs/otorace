d0fb29a2e7ae56678a183861637dc7425bb363c3
Daan Devos <daan.devos@vintage.eu>
2014-09-27 19:56:44 +0200

    socket auth

--- server.js
+++ server.js
@@ -2,14 +2,109 @@
 'use strict';
 
 function handleHTTP(req, res) {
-	res.end('hello world');
+	if(req.method === 'GET') {
+		if(req.url === '/') {
+			req.url = 'index.html';
+			static_files.serve(req, res);
+		}else {
+			res.writeHead(403);
+		}
+	}else if(req.method === 'POST') {
+		if(req.url === '/login') {
+			var body = '';
+
+			req.on('data', function(chunk) {
+				body += chunk;
+			});
+
+			req.on('end', function() {
+				var postJSON = {};
+
+				try {
+					postJSON = JSON.parse(body);
+				}catch(err) {
+					res.writeHead(401);
+					res.end();
+				}
+
+				if(postJSON.username && verifyUsername(postJSON.username)) {
+					clients[postJSON.username] = {}; // already reserve this username
+
+					res.writeHead(200, {'Content-Type': 'application/json'});
+
+					var profile = {
+				    username: postJSON.username
+				  };
+
+				  var token = jwt.sign(profile, jwt_secret, { expiresInMinutes: 60*5 });
+					res.end(JSON.stringify({token: token}));
+				}else {
+					res.writeHead(401);
+					res.end();
+				}
+			});
+
+		}else {
+			res.writeHead(403);
+		}
+	}else {
+		res.writeHead(403);
+	}
+}
+
+function verifyUsername(name) {
+	console.log('verify');
+	console.log(clients);
+
+	if(!clients[name]) {
+		return true;
+	}
+	return false;
+}
+
+function handleIO(socket) {
+	clients[socket.decoded_token.username] = socket;
+
+	socket.on('disconnect', function() {
+		console.log('disconnect');
+		delete clients[socket.decoded_token.username];
+
+	});
+
+	/*socket.broadcast.emit('new user', {});
+
+	function handlePosition(data) {
+		socket.broadcast.emit('position', data);
+	}
+
+	socket.on('position', handlePosition);*/
 }
 
 var http = require('http'),
 	http_serv = http.createServer(handleHTTP),
+
 	host = 'localhost', // or find IP address of host computer acting as server (terminal: ifconfig -> information under en1)
-	port = 3000;
+	port = 3000,
+
+	node_static = require('node-static'),
+	static_files = new node_static.Server(__dirname + '/app'),
+
+	io = require('socket.io').listen(http_serv),
+	io_jwt = require('socketio-jwt'),
+
+	jwt = require('jsonwebtoken'),
+	jwt_secret = 'token',
+
+	clients = {};
+
 
 http_serv.listen(3000, host);
+
+io.use(io_jwt.authorize({
+	secret: jwt_secret,
+	handshake: true
+}));
+
+io.on('connection', handleIO);
 
 module.exports = http_serv;
--- test/server.spec.js
+++ test/server.spec.js
@@ -3,19 +3,175 @@
 
   var assert = require('chai').assert,
     request = require('supertest'),
-    app = require('../server');
+    app = require('../server'),
+
+    io = require('socket.io-client'),
+
+    socketURL = 'http://0.0.0.0:3000',
+    options ={
+      transports: ['websocket'],
+      'force new connection': true
+    };
 
   describe('/', function() {
-    it('should return 200', function(done) {
+    it('respond with html', function(done) {
       request(app)
         .get('/')
+        .expect('Content-Type', 'text/html')
         .expect(200, done);
     });
-
-    it('should say "hello world"', function(done) {
-      request(app)
-        .get('/')
-        .expect('hello world', done);
-    });
   });
+
+  describe('/login', function() {
+    it('should fail if no username is provided', function(done) {
+      request(app)
+        .post('/login')
+        .expect(401, done);
+    });
+
+    describe('socket.io', function() {
+      before(function(done) {
+        request(app)
+          .post('/login')
+          .send({
+            username: 'daan'
+          }).end(function(err, res, body) {
+            this.token = res.body.token;
+            this.socket1 = io.connect(socketURL, {
+              'forceNew': true,
+              'query': 'token=' + this.token
+            });
+            done();
+          }.bind(this));
+      });
+
+      it('should do handshake and connect', function(done) {
+        this.socket1.on('connect', function() {
+          done();
+        });
+      });
+
+      it('should fail without token', function(done) {
+        var socket = io.connect(socketURL, {
+          'forceNew': true,
+          'query': 'token=bb'
+        });
+
+        socket.on('error', function(err) {
+          assert.equal(err.type, 'UnauthorizedError');
+          done();
+        });
+
+      });
+
+      it('should fail if user with same name tries to connect', function(done) {
+        request(app)
+          .post('/login')
+          .send({
+            username: 'daan'
+          })
+          .expect(401, done);
+      });
+
+      it('should inform other clients when disconnected', function(done) {
+        this.socket1.on('disconnect', function(err) {
+          //setTimeout(function() {
+            done();
+          //}, 500);
+        });
+
+        this.socket1.disconnect();
+      });
+
+      it('have the name available', function(done) {
+        request(app)
+          .post('/login')
+          .send({
+            username: 'daan'
+          })
+          .expect(200, done);
+      });
+    });
+
+    /*describe('interaction between clients', function() {
+
+      before(function(done) {
+        request(app)
+          .post('/login')
+          .send({
+            username: 'bob'
+          }).end(function(err, res, body) {
+            this.token = res.body.token;
+            done();
+          }.bind(this));
+      });
+
+      it('should succesfully connect a second user', function(done) {
+        var socket = io.connect(socketURL, {
+          'forceNew': true,
+          'query': 'token=' + this.token
+        });
+
+        socket.on('connect', function() {
+          done();
+        });
+      });
+
+
+    });*/
+  });
+
+  /*describe('socket.io', function() {
+    before(function() {
+      this.client1 = io.connect(socketURL, options);
+      this.client2 = io.connect(socketURL, options);
+      this.client3 = io.connect(socketURL, options);
+
+      this.client1.on('connect', function(data) {
+        this.client1.emit('user', {
+          name: 'daan'
+        });
+      }.bind(this));
+
+      this.client2.on('connect', function(data) {
+        this.client2.emit('user', {
+          name: 'jos'
+        });
+      }.bind(this));
+
+      this.client3.on('connect', function(data) {
+        this.client3.emit('user', {
+          name: 'jos'
+        });
+      }.bind(this));
+    });
+
+    it('should refuse connection when username is already taken', function(done) {
+      this.client3.on('new user', function(data) {
+
+        done();
+      });
+
+
+    });
+
+    it('should inform other clients connection', function(done) {
+      this.client1.on('new user', function(data) {
+        done();
+      });
+    });
+
+    it('should receive x and y positions', function(done) {
+      this.client1.emit('position', {
+        x: 200,
+        y: 300
+      });
+
+      this.client2.on('position', function(data) {
+        assert.equal(data.x, 200);
+        assert.equal(data.y, 300);
+        done();
+      });
+    });
+  });*/
 })();
